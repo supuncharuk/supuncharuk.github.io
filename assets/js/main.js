df20e276331d4e664920bf41600a9e94c941fb3c
SupunCharuk2001 <pdscharuka@gmail.com>
2023-12-07 23:13:49 +0530

    making changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,7 @@
   on('click', '.mobile-nav-toggle', function(e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
+    this.classList.toggle('bx-x')
   })
 
   /**
